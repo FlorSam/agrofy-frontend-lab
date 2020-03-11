896efba956012c68dd60ffff2aa2549e8e5e835f
FlorSam <florsambucaro@gmail.com>
2020-03-11 02:30:22 -0300

    commit

--- week3-4/src/pages/Pokedex/Pokedex.js
+++ week3-4/src/pages/Pokedex/Pokedex.js
@@ -2,50 +2,42 @@ import React, { useEffect, useState } from 'react';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import PokemonCard from '../../components/PokemonCard/PokemonCard';
 
-const Pokedex = () => (
+const Pokedex = () => {
+  const [pokemon, setPokemon] = useState([]);
 
-  <div className="pokedex page">
-    <h1>Pokedex</h1>
-    <p>holii</p>
-    <SearchBar />
-  </div>
-)
+  const api = () => {
+    let promises = [];
+    for (let i = 1; i < 151; i++) {
+      const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+      promises.push(fetch(url).then(resp => resp.json()));
+    }
+    Promise.all(promises).then(list => {
+      console.log(list);
+      setPokemon(list.map(poke => {
+        return {
+          id: poke.id,
+          name: poke.name,
+          img: poke.sprites["front_default"],
+          type: poke.types.map(type => type.type.name).join(", ")
 
-const [PokemonCards, setPokemonCards] = useState([]);
-const [Search, setSearch] = useState ("");
-
-useEffect = (() => {
-  api();
-}, [Search]);
-
-
-const api = async () => {
-  let pokemon = [];
-  let promises = [];
-  const poke = [];
-
-  for (let i = 1; i < 151; i++) {
-    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    promises.push(fetch(url).then(resp => resp.json()));
-  }
-  Promise.all(promises).then(list => {
-    pokemon = list;
-    console.log(list);
-    setPokemonCards(list);
-  
-    const { id, name, image, type } = poke;
-    console.log(poke);
-  
-      poke.map(pokemon.map(poke => {
-         { id, name, image, type };
-        <PokemonCard
-          name={poke.name}
-          img={poke.sprites.front_default}
-          type={poke.types.map(type => type.type.name).join(", ")}
-        />
-    
+        };
       }))
-  })
+    });
+  };
+  useEffect(
+    () => {
+       api();
+    }, []);
+
+return <div className="pokedex page">
+
+  <h1>Pokedex</h1>
+  <p>holii</p>
+  <SearchBar />
+  {
+    pokemon.map(poke => <PokemonCard name={poke.name} img={poke.img} type={poke.types} />)
+  }
+</div>
 }
 
 export default Pokedex;
